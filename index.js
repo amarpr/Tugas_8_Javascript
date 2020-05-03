4323341243e3cf817b9ca9ab5afd2cd5f343007f
Muhamar <amararrasyid@gmail.com>
2020-05-03 13:00:39 +0700

    Tugas 8 Javascript
    Mengumpulkan Tugas Niomic

---
+++ index.js
@@ -0,0 +1,11 @@
+function saya() {
+    var namaSaya = {
+        nama_depan: 'Muhamar',
+        nama_belakang: 'Ar-Rasyid'
+    }
+
+    namaSaya.nama_depan = 'Nanda';
+    console.log(namaSaya)
+}
+
+saya()
